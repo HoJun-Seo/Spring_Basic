1e2f81eecc8e2761940ae87ae63c42feaf7bae17
HoJun-Seo <ghwns6659@gmail.com>
2022-02-10 23:31:06 +0900

    Spring_Basic(OOP)

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -9,15 +11,23 @@ import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
-
-    // 구현체 클래스에서 작성해준 구현체 생성자 클래스를 통해
-    // Repository 구현체인 MemoryMemberRepository 객체를 주입해준다.
+    
+    
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
-    // 생성자를 통해 주문 구현체에 Repository 와 DiscountPolicy 인터페이스의 구현체를 주입한다.
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    private DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
+        // 정액 할인 정책 -> 정률 할인 정책으로 변경
     }
 }
